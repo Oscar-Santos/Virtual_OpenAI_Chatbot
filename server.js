1289fb275d8d32d022be49a622c0c154e9ed0512
Oscar Santos <oscar303data@gmail.com>
2023-07-30 17:10:24 -0600

    initial setup of the openai api call

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+// importing open ai node.js confuguration
+// import { Configuration, OpenAIApi } from "openai";
+
+const OpenAI = require('openai');
+const { Configuration, OpenAIApi } = OpenAI
+
+//
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
@@ -6,7 +13,14 @@ const cors = require('cors')
 const app = express()
 const port = 3000
 
-
+// setup of the API call
+const configuration = new Configuration({
+    organization: "org-eYnp3AUeccZLuh7xaZTNSpCE",
+    apiKey: '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',
+});
+const openai = new OpenAIApi(configuration);
+// const response = await openai.listEngines();
+//
 
 app.post('/',(req, res) => {
     res.json({ message: "Hello  crazy World!" })
