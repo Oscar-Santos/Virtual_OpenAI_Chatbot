e69f57919883fe4a6be9dea029133b180ed80b2c
Oscar Santos <oscar303data@gmail.com>
2023-07-30 16:41:45 -0600

    setup function App on App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 
+import { useState } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
-}
-
-export default App;
+export default function App() {
+    const [message, setMessage] = useState("")
+    const [response, setResponse] = useState("")
+    
+    function handleSubmit(e) {
+        e.preventDefault();
+        fetch("http://localhost:3000", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ message })
+        })
+        .then(res => res.json())
+        .then(data => setResponse(data.message))
+        .catch(err => console.log(err))
+    }
+    
+    return (
+        <div className="app">
+            <form onSubmit={handleSubmit}>
+                <input
+                type="text"
+                placeholder="Enter a message..."
+                value={message}
+                onChange={e => setMessage(e.target.value)}
+                />
+                <button type="submit">Send</button>
+            </form>
+            <p>{response}</p>
+        </div>
+    );
+    }
