f0644293cdb4fe3210d6a1bf16a0eec8bf281a0f
Oscar Santos <oscar303data@gmail.com>
2023-08-01 06:00:06 -0600

    refactor general styling

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,51 @@
-
+import Image30 from '../images/image_30.png';
 export default function About() {
 
     return (
         <div>
 
-<body>
-<div className="about">
-            <header>
-            <h1> About ChatBot AI</h1> 
-               
-            </header>
-            <main>
-                <section>
-                    <h2>Introduction</h2>
-                    <p>
-                        ChatBotAI is an intelligent chatbot application powered by cutting-edge natural language processing models from OpenAI. It allows users to interact with the chatbot by asking questions on various topics and receiving informative and accurate answers.
-                    </p>
-                </section>
+  <body>
+    <div class="about">
+        <header>
+            <h1>About OpenAI</h1>
+         </header>
+       
+        <main>
+        <img src={Image30} width="450px" height="120px" class="center" alt="" />
+            <section>
+            
+                <h2>Introduction</h2>
+                <p>
+                    OpenAI is a research organization and technology company that focuses on artificial intelligence (AI) and machine learning advancements. It was founded with a mission to ensure that AI benefits all of humanity and to build safe, powerful, and beneficial AI technologies.
+                </p>
+            </section>
 
-                <section>
+            <section>
+                <h2>History</h2>
+                <p>
+                    OpenAI was established in December 2015 by a group of influential tech visionaries and AI researchers, including Elon Musk, Sam Altman, Greg Brockman, Ilya Sutskever, John Schulman, and Wojciech Zaremba, among others. The idea behind OpenAI was to create a non-profit organization that would promote and develop AI in a way that prioritizes positive societal impact over profit motives.
+                </p>
+                <p>
+                    Over time, OpenAI evolved its organizational structure and funding model. In 2019, OpenAI became a for-profit company, OpenAI LP, with the goal of attracting additional investment to support its ambitious research and development efforts. Despite this change, the company's commitment to its core principles remained unwavering.
+                </p>
+            </section>
+
+            <section>
+                    <h2>ChatBot AI</h2>
+                    <p>
+                        ChatBotAI is an intelligent chatbot application powered by cutting-edge natural language processing models from OpenAI. It allows users to interact with the chatbot by asking questions on various topics and receiving informative and accurate answers. This ChatBot AI uses the "Model Test-Davinci 003".
+                    </p>
+            </section>
+
+            <section>
                     <h2>How it Works</h2>
                     <p>
                         ChatBotAI utilizes the OpenAI API, which enables it to understand natural language and generate human-like responses. The application sends user queries to the API, which processes the input and returns the chatbot's responses based on advanced language models.
                     </p>
-                </section>
+             </section>
 
-                <section>
+
+            <section>
                     <h2>Features</h2>
                     <p>
                         ChatBotAI offers the following features:
@@ -38,19 +58,38 @@ export default function About() {
                     </p>
                 </section>
 
-                <section>
+            <section>
+                <h2>Contributions to AI Research</h2>
+                <p>
+                    OpenAI has been at the forefront of AI research, achieving numerous breakthroughs in various domains. One of its most significant contributions was the development of GPT (Generative Pre-trained Transformer) language models. GPT-2, released in 2019, showcased the remarkable ability of language models to generate human-like text and sparked discussions on the potential misuse of such technology.
+                </p>
+                <p>
+                    Building on the success of GPT-2, OpenAI introduced GPT-3, which took natural language processing to new heights. GPT-3, released in 2020, boasted an impressive 175 billion parameters, making it one of the largest and most powerful language models at the time. Its versatility and capacity for understanding context led to a wide range of applications, from chatbots to content generation.
+                </p>
+            </section>
+
+            <section>
+                <h2>Mission and Ethics</h2>
+                <p>
+                    OpenAI's mission revolves around ensuring that artificial general intelligence (AGI) benefits all of humanity. AGI refers to highly autonomous systems that outperform humans at most economically valuable work. OpenAI aims to build AGI that is safe, beneficial, and aligned with human values while also advocating for the responsible development and deployment of AI technologies worldwide.
+                </p>
+                <p>
+                    The organization actively engages in research and policy advocacy to address AI's potential challenges, including issues related to fairness, transparency, and accountability. OpenAI remains committed to long-term safety and is dedicated to preventing uses of AI or AGI that could harm humanity or concentrate power in the hands of a few.
+                </p>
+            </section>
+
+            <section>
                     <h2>Privacy and Security</h2>
                     <p>
                         At ChatBotAI, user privacy and data security are of utmost importance. All user queries are processed anonymously, and no personally identifiable information is stored. We ensure that your interactions with the chatbot remain private and secure.
                     </p>
-                </section>
-            </main>
-            <footer>
-                <p>&copy; 2023 OpenAI. All rights reserved.</p>
-            </footer>
-        </div> 
-</body>  
-            
+            </section>
+        </main>
+        <footer>
+            <p>&copy; 2023 OpenAI. All rights reserved.</p>
+        </footer>
+    </div>
+</body>          
         </div>
     )
 }
